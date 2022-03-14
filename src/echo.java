ac53bfab71b1ffe66b720398dab5398599f20d53
sonmyungwon <45677898+sonmyungwon@users.noreply.github.com>
2022-03-14 19:12:41 +0900

    echo

---
+++ src/echo.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class echo {
+	public static void main(String[] args) {
+	Scanner string;
+	string = new Scanner(System.in);
+	System.out.print("input:");
+	String line= string.nextLine();
+	System.out.println(line);
+	
+	}
+}
