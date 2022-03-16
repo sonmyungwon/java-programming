de9c45feca38b2592e258bdf737028527dfa5bf7
sonmyungwon <45677898+sonmyungwon@users.noreply.github.com>
2022-03-17 02:10:08 +0900

    assignment xmanagement

--- src/menuManagement.java
+++ src/menuManagement.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 public class menuManagement {
 	public static void main (String args[]) {
 		int com = 0;
+		String scheduleName = null;
+		String scheduleDate= null;
+		String place= null;
+		String friend= null;
+		String pnum= null;
 		Scanner input= new Scanner(System.in);
 		while(com!=6){
 		System.out.println("1. Add Schedule");
@@ -16,14 +21,46 @@ public class menuManagement {
 		com = input.nextInt();
 		switch(com) {
 		case 1 : 		
-			System.out.println("schedule name");		
-			String schedule = input.next();
-
-			System.out.println("2. Delate Schedule");
-			System.out.println("3. Edit Schedule");
-			System.out.println("4. View Schedule");
-			System.out.println("5. Exit");
-		}}
+			System.out.println("schedule name: ");		
+			scheduleName = input.next();
+			System.out.println("schedule date: ");
+			scheduleDate = input.next();
+			System.out.println("meeting place: ");
+			place = input.next();
+			System.out.println("With whom: ");
+			friend = input.next();
+			System.out.println("contact number: ");
+			pnum = input.next();
+			break;
+		
+		case 2:
+			scheduleName="";
+			scheduleDate="";
+			place="";
+			friend="";
+			pnum="";
+			break;
+		case 3:
+			System.out.println("schedule name: "+scheduleName);		
+			scheduleName = input.next();
+			System.out.println("schedule date: "+scheduleDate);
+			scheduleDate = input.next();
+			System.out.println("meeting place: "+ place);
+			place = input.next();
+			System.out.println("With whom: "+friend);
+			friend = input.next();
+			System.out.println("contact number: "+pnum);
+			pnum = input.next();
+			break;
+		case 4:
+			System.out.println("schedule name:  " + scheduleName);		
+			System.out.println("schedule date:  " + scheduleDate);
+			System.out.println("meeting place:  " + place);
+			System.out.println("With whom:  " + friend);
+			System.out.println("contact number:  " + pnum);
+		
+			}
+		}
 	}
 	
 }
