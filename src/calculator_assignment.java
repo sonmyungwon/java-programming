2abeb33f167f8e70abc1a0d07ee34d62809f124c
sonmyungwon <45677898+sonmyungwon@users.noreply.github.com>
2022-03-17 01:44:33 +0900

    final-2

--- src/calculator_assignment.java
+++ src/calculator_assignment.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class calculator_assignment {
 	public static void main(String args[]) {
 	System.out.print("Please enter a number1:");
-	Scanner number1= new Scanner(System.in);
-	int num1 = number1.nextInt();
+	Scanner input= new Scanner(System.in);
+	int num1 = input.nextInt();
 	System.out.print("Please enter a number2:");
-	Scanner number2= new Scanner(System.in);
-	int num2 = number2.nextInt();
+	int num2 = input.nextInt();
 	System.out.print("Please enter Operator:");
-	Scanner Operator= new Scanner(System.in);
-	String Oper = Operator.nextLine();
+	String Oper = input.next();
 	int ans =0;
 	switch(Oper){
 		case "*" : ans =num1*num2;
--- src/menuManagement.java
+++ src/menuManagement.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 
 public class menuManagement {
 	public static void main (String args[]) {
+		int com = 0;
+		Scanner input= new Scanner(System.in);
+		while(com!=6){
 		System.out.println("1. Add Schedule");
 		System.out.println("2. Delate Schedule");
 		System.out.println("3. Edit Schedule");
 		System.out.println("4. View Schedule");
-		System.out.println("5. Exit");
+		System.out.println("5. show a menu");
+		System.out.println("6. Exit");
+		System.out.print("Please enter a number:");
+		com = input.nextInt();
+		switch(com) {
+		case 1 : 		
+			System.out.println("schedule name");		
+			String schedule = input.next();
+
+			System.out.println("2. Delate Schedule");
+			System.out.println("3. Edit Schedule");
+			System.out.println("4. View Schedule");
+			System.out.println("5. Exit");
+		}}
 	}
+	
 }
