e312b120c9e52491bc7288fab439caaaa98d88b8
sonmyungwon <45677898+sonmyungwon@users.noreply.github.com>
2022-03-17 01:00:29 +0900

    final

--- src/day_sec_trans.java
+++ src/day_sec_trans.java
@@ -10,23 +10,17 @@ public class day_sec_trans {
 		if(time>60*60*12) {
 			day= time/(60*60*24);
 			time=time%(60*60*24);
-			System.out.println(time);
 			if (time>60*60) {
 				hour = time/(60*60);
 				time=time%(60*60);
-				System.out.println(time);
 				}
 				if (time>60) {
 					min = time/(60);
 					time=time%60;
-					System.out.println(time);
 					}
-					else {
-						sec = time;
-						System.out.println(time);
-						}
+					else sec = time;
 		if(day<365) {
-			System.out.print("일년이하입니다.");
+			System.out.println("less than a year");
 		}
 		System.out.print(day +"day  ");
 		System.out.print(hour+"hour  ");
